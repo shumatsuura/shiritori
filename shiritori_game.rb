e607c4550899225aa7a6833bab9e5199ce7705cf
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-03-16 17:35:17 +0900

    specを実行

--- shiritori_game.rb
+++ shiritori_game.rb
@@ -1,8 +1,8 @@
 class ShiritoriGame
   def self.rule
     rule_text = ''
-    rule_text << "全てカタカナで入力してください\n"
-    rule_text << "最後に 'ン'がついたら負けです\n"
+    rule_text << "すべてカタカナで入力してください\n"
+    rule_text << "最後に 'ン' がついたら負けです\n"
     rule_text << "同じ単語を使ったら負けです\n"
     rule_text
   end
--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -1,7 +1,7 @@
 require './shiritori_game'
 
 RSpec.describe ShiritoriGame do
-  describe './rule' do
+  describe '.rule' do
     it 'シリトリのルールが表示される' do
       expect(ShiritoriGame.rule).to eq("すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n")
     end
