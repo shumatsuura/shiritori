d3aa37eb39e3b1dfd381d185187be59f3ec10971
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-03-16 17:28:51 +0900

    specファイル作成

---
+++ shiritori_game_spec.rb
@@ -0,0 +1,9 @@
+require './shiritori_game'
+
+RSpec.describe ShiritoriGame do
+  describe './rule' do
+    it 'シリトリのルールが表示される' do
+      expect(ShiritoriGame.rule).to eq("すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n")
+    end
+  end
+end
